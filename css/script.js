984c8fad3464123bf18f575269b8271fc33d7a82
Leo <leoamby@gmail.com>
2019-07-13 14:12:31 +0300

    outline the variables and functions required for the js file

--- css/script.js
+++ css/script.js
@@ -2,26 +2,28 @@ var CC;
 var YY;
 var MM;
 var DD;
+var dOb;
+var femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+function inputs(){
 
-
-function gender(a, b){
-    var a = male;
-    var b = female;
 }
- 
-function dayOfBirth() {
-    var dayOfBirth;
-    dayOfBirth = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+
+function dayOfBirth(){
+    inputs();
+    dOb = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
 }
-femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-
-
-dayOfBirth (){
- if (dayOfBirth === 0 && gender === male)
-  document.write("you were born on " + dayOfBirth. Your Akan name is )
+function gender(){
+    //incomplete
 }
+
+function validate(){
+    dayOfBirth();
+    gender();
+    //incomplete
+}
+
 
