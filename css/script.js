49b60d424b1d5c5f2fd492d277a4220dd68f22c4
Leo <leoamby@gmail.com>
2019-07-13 10:43:42 +0300

    add the date function in the script.js file

--- css/script.js
+++ css/script.js
@@ -1,5 +1,13 @@
 function gender(a, b){
     var a = male;
     var b = female;
-    if
+}
+ 
+function dayOfWeek() {
+    results dayOfWeek;
+    dayOfWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    var CC = century;
+    var YY = year;
+    var MM = month;
+    var DD = date;
 }
