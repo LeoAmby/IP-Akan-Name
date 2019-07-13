41f3df4b4e398b3986978f5e00e151ad51ff0299
Leo <leoamby@gmail.com>
2019-07-13 14:46:47 +0300

    elaborate on the dayOfBirth and radioButtons functions

--- css/script.js
+++ css/script.js
@@ -3,6 +3,7 @@ var YY;
 var MM;
 var DD;
 var dOb;
+var gender; 
 var femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -12,23 +13,30 @@ function inputs(){
     YY = parseInt(document.getElementById("year")).value;
     DD = parseInt(document.getElementById("dateOfBirth")).value;
     MM = parseInt(document.getElementById("monthOfBirth")).value;
-
-
 }
 
 function dayOfBirth(){
     inputs();
     dOb = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+    return (Math.floor(dOb));
 }
 
-function gender(){
-    //incomplete
+function radioButtons(){
+    var gender = document.getElementsByName("gridRadios");
+    if (gridRadios[0].checked == true) {
+        var gender = "Male";
+    } else if (gridRadios[1].checked == true){
+        var gender = "Female";
+    } else if (gridRadios[2].checked == true){
+        var gender = "Unidentified";
+    }
 }
 
 function validate(){
     dayOfBirth();
-    gender();
+    radioButtons();
     //incomplete
+    if 
 }
 
 
